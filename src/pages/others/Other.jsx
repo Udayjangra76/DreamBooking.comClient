c7c7d85601ac530a50b67a836f50f02fc21480b3
Uday Jangra <85983169+Udayjangra76@users.noreply.github.com>
2023-06-13 17:04:13 +0530

    Update Other.jsx

--- src/pages/others/Other.jsx
+++ src/pages/others/Other.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../../components/navbar/Navbar.jsx'
 import Footer from '../../components/footer/Footer.jsx';
 import Header from './../../components/header/Header.jsx';
-import './others.css'
+import './Others.css'
 
 const other = () => {
     return (
