904573d145d7bebc81b09d9c2816b2d211d2bc76
Uday Jangra <85983169+Udayjangra76@users.noreply.github.com>
2023-06-13 17:04:34 +0530

    Update Register.jsx

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./register.css";
+import "./Register.css";
 import { BASE_URL } from "../../helper.js";
 
 const Register = () => {
